78bac88e4be85509f0912b1b4ab4ff829fe77df3
who am i <62942231+sachira-madhushan@users.noreply.github.com>
2024-06-23 09:20:11 +0530

    jquery btn click

--- js/jquery.js
+++ js/jquery.js
@@ -5,3 +5,13 @@ $('#show-btn').click(function () {
 $('#hide-btn').click(function () {
     $('#view').hide();  
 })
+
+$('#home').click(function () {
+    $('#iframe-view').attr('src','../html/home.html');  
+})
+$('#about').click(function () {
+    $('#iframe-view').attr('src','../html/about.html');  
+})
+$('#details').click(function () {
+    $('#iframe-view').attr('src','../html/details.html');  
+})
